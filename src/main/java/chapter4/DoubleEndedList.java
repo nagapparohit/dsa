26ad379db5044edca8d85ec367d4de286ae2140c
rohit nagappa <rnagappa35@gmail.com>
2020-07-17 23:30:20 +0530

    designed Double ended list using linked list

---
+++ src/main/java/chapter4/DoubleEndedList.java
@@ -0,0 +1,18 @@
+package chapter4;
+
+public class DoubleEndedList<T> extends LinkedList<T> {
+
+	private Node<T> tail;
+	
+	public Node<T> getTail() {
+		return tail;
+	}
+	
+	public void addAtEnd(T data) {
+		Node<T>  newNode = new Node<T>(data);
+		this.tail.setNextNode(newNode);
+		this.tail = newNode;
+		
+	}
+	
+}
