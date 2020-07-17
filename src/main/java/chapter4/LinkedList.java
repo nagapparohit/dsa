22b19fd55a74c72907c680705bb5b4226bd80400
rohit nagappa <rnagappa35@gmail.com>
2020-07-17 09:23:41 +0530

    implemented linkedList with generics

---
+++ src/main/java/chapter4/LinkedList.java
@@ -0,0 +1,70 @@
+package chapter4;
+
+
+public class LinkedList<T> {
+
+	private Node<T> head;
+	
+	public LinkedList() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Node<T> getHead() {
+		return head;
+	}
+	
+	public void setHead(Node<T> head) {
+		this.head = head;
+	}
+	
+	public void addAtStart(T data) {
+		Node<T> newNode = new Node<T>(data);
+		newNode.setNextNode(this.head);
+		this.head = newNode;
+	}
+	
+	public Node<T> deleteAtStart() {	
+		Node<T> toDel = this.head;
+		this.head = this.head.getNextNode();
+		return toDel;
+	}
+	
+	public Node<T> find(T data){
+		Node<T> curr = this.head;
+		while(curr != null) {
+			if(curr.getClass().equals(data)) {
+				return curr;
+			}
+			curr = curr.getNextNode();
+		}
+		return null;
+	}
+	
+	public int length() {
+		if(head == null) {
+			return 0;
+		}
+		int length = 0;
+		Node<T> curr = this.head;
+		while(curr != null) {
+			length += 1;
+			curr = curr.getNextNode();
+		}
+		return length;
+	}
+	
+	public boolean isEmpty() {
+		return this.head == null;
+	}
+	
+	@Override
+	public String toString() {
+		String res = "";
+		Node<T> curr = this.head;
+		while(curr != null) {
+			res += curr + ",";
+			curr = curr.getNextNode();
+		}
+		return res;
+	}
+}
